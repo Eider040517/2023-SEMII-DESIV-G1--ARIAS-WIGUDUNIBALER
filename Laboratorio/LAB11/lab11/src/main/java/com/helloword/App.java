fa9ce1a06fb495f5cd7249ed9a9c603601d850d0
Wigudunibaler Arias <wigudunibalera@gmail.com>
2023-12-01 02:05:45 -0500

    Update Laboratorio 11

---
+++ Laboratorio/LAB11/lab11/src/main/java/com/helloword/App.java
@@ -0,0 +1,12 @@
+package com.helloword;
+
+/**
+ * Hello world!
+ */
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
